c07c941f656edf5b6b8364af00b065b587c4cfa3
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-25 13:24:32 +0300

    Adjusted configuration of Spring Boot application to make it 'war' deployable

--- src/main/java/org/laplas/boot/SpringBootWebApplication.java
+++ src/main/java/org/laplas/boot/SpringBootWebApplication.java
@@ -3,7 +3,9 @@ package org.laplas.boot;
 import org.laplas.boot.property.DefaultPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * Main Spring Boot configuration class which bootstraps the application.
@@ -13,12 +15,21 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @EnableConfigurationProperties(value = DefaultPropertySource.class)
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     /**
      * Main application entry point to launch the application.
      */
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    /**
+     * Along with {@link SpringBootServletInitializer} class is a step towards
+     * creation of a traditional deployable 'war' file.
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootWebApplication.class);
     }
 }
